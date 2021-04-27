650550931710c4c3a28f98629449f4e690375d4d
Abdul Wahab <abdul.wahab3470@gmail.com>
2021-04-28 03:10:00 +0500

    Add files via upload

---
+++ conn.php
@@ -0,0 +1,16 @@
+<?php
+
+
+$con = mysqli_connect('localhost','root','','php-crud');
+
+//mysqli_select_db($con,'crudtable');
+
+// if($con)
+// {
+//     echo "Connected";
+// }
+// else{
+//     echo "not connected";
+// }
+
+?>
